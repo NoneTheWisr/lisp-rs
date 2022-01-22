86ff737a43b82d67a0aefd1af078bfcde41ad7bf
NoneTheWisr <12655815+NoneTheWisr@users.noreply.github.com>
2022-01-22 23:54:39 +0400

    implement lexer prototype for (, )

---
+++ src/lexer.rs
@@ -0,0 +1,52 @@
+use std::iter::Peekable;
+
+use crate::token::Token;
+
+struct Lexer<I: Iterator<Item = char>> {
+    source: Peekable<I>
+}
+
+impl<I> Iterator for Lexer<I>
+where
+    I: Iterator<Item = char>
+{
+    type Item = Result<Token, ()>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.source.next() {
+            Some(next_char) => {
+                match next_char {
+                    '(' => Some(Ok(Token::LParen)),
+                    ')' => Some(Ok(Token::RParen)),
+                    _ => Some(Err(()))
+                }
+            }
+            None => None
+        }
+    }
+}
+
+impl<I> From<I> for Lexer<I>
+where
+    I: Iterator<Item = char>,
+{
+    fn from(source: I) -> Self {
+        Lexer { source: source.peekable() }
+    }
+}
+
+impl<'a> From<&'a str> for Lexer<std::str::Chars<'a>> {
+    fn from(source: &'a str) -> Self {
+        Lexer { source: source.chars().peekable() }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_string_slice() {
+        assert_eq!(Lexer::from("()").collect::<Vec<_>>(), vec![Ok(Token::LParen), Ok(Token::RParen)]);
+    }
+}
