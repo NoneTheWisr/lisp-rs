0908d92a9b38b39f592f45dcab3b9794bd847770
NoneTheWisr <12655815+NoneTheWisr@users.noreply.github.com>
2022-02-02 14:39:34 +0400

    pass env as an argument to builtins

--- src/eval/value.rs
+++ src/eval/value.rs
@@ -2,11 +2,13 @@ use std::rc::Rc;
 
 use num_bigint::BigInt;
 
+use super::env::Env;
+
 #[derive(Clone)]
 pub enum Value {
     Int(BigInt),
     Str(String),
-    Fun(Rc<dyn Fn(Vec<Value>) -> Result<Value, ()>>),
+    Fun(Rc<dyn Fn(Vec<Value>, &mut Env) -> Result<Value, ()>>),
     Nil,
     None,
 }
