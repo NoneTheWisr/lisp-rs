f82ba387e2a978da1fa887f4d474aa4af6f64510
NoneTheWisr <12655815+NoneTheWisr@users.noreply.github.com>
2022-02-02 17:21:42 +0400

    add cmp and * builtins

--- src/eval/builtins.rs
+++ src/eval/builtins.rs
@@ -9,6 +9,10 @@ impl Default for Env {
     fn default() -> Self {
         Self::new(vec![
             ("+".into(), Value::Fun(Rc::new(add))),
+            ("*".into(), Value::Fun(Rc::new(add))),
+            ("=".into(), Value::Fun(Rc::new(eq))),
+            (">".into(), Value::Fun(Rc::new(gt))),
+            ("<".into(), Value::Fun(Rc::new(lt))),
             ("def".into(), Value::Fun(Rc::new(def))),
         ].into_iter().collect())
     }
@@ -16,19 +20,19 @@ impl Default for Env {
 
 macro_rules! assert_arg_count {
     ($args:ident, $count:pat) => {
-        if !matches!($args.len(), pat) {
+        if !matches!($args.len(), $count) {
             return Err(());
         }
     };
     ($args:ident, $count:pat, $err:expr) => {
-        if !matches!($args.len(), pat) {
+        if !matches!($args.len(), $count) {
             return Err($err);
         }
     };
 }
 
 pub(super) fn add(args: Vec<Value>, _: &mut Env) -> Result<Value, ()> {
-    assert_arg_count!(args, (1..));
+    assert_arg_count!(args, (2..));
 
     use Value::*;
     match args[0] {
@@ -42,6 +46,56 @@ pub(super) fn add(args: Vec<Value>, _: &mut Env) -> Result<Value, ()> {
             .map(|v| if let Str(v) = v { Ok(v) } else { Err(()) })
             .collect::<Result<_, _>>()
             .map(Str),
+        _ => Err(()),
+    }
+}
+
+pub(super) fn mul(args: Vec<Value>, _: &mut Env) -> Result<Value, ()> {
+    assert_arg_count!(args, (2..));
+
+    use Value::*;
+    match args[0] {
+        Int(_) => args
+            .into_iter()
+            .map(|v| if let Int(v) = v { Ok(v) } else { Err(()) })
+            .product::<Result<_, _>>()
+            .map(Int),
+        Str(_) => {
+            assert_arg_count!(args, 2);
+            match args.into_iter().next_tuple().unwrap() {
+                (Value::Str(str), Value::Int(int)) => {
+                    let int: usize = int.try_into().map_err(|_| ())?;
+                    Ok(Value::Str(str.repeat(int)))
+                }
+                _ => Err(()),
+            }
+        }
+        _ => Err(()),
+    }
+}
+
+pub(super) fn eq(args: Vec<Value>, _: &mut Env) -> Result<Value, ()> {
+    assert_arg_count!(args, (2..));
+
+    Ok(Value::Bool(args.into_iter().all_equal()))
+}
+
+pub(super) fn gt(args: Vec<Value>, _: &mut Env) -> Result<Value, ()> {
+    assert_arg_count!(args, 2);
+
+    match args.into_iter().next_tuple().unwrap() {
+        (Value::Int(lhs), Value::Int(rhs)) => Ok(Value::Bool(lhs > rhs)),
+        (Value::Str(lhs), Value::Str(rhs)) => Ok(Value::Bool(lhs > rhs)),
+        _ => Err(()),
+    }
+}
+
+pub(super) fn lt(args: Vec<Value>, _: &mut Env) -> Result<Value, ()> {
+    assert_arg_count!(args, 2);
+
+    match args.into_iter().next_tuple().unwrap() {
+        (Value::Int(lhs), Value::Int(rhs)) => Ok(Value::Bool(lhs < rhs)),
+        (Value::Str(lhs), Value::Str(rhs)) => Ok(Value::Bool(lhs < rhs)),
         _ => Err(()),
     }
 }
--- src/eval/value.rs
+++ src/eval/value.rs
@@ -14,3 +14,16 @@ pub enum Value {
     Symbol(String),
     None,
 }
+
+impl PartialEq for Value {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Int(lhs), Self::Int(rhs)) => lhs == rhs,
+            (Self::Str(lhs), Self::Str(rhs)) => lhs == rhs,
+            (Self::Bool(lhs), Self::Bool(rhs)) => lhs == rhs,
+            (Self::Symbol(lhs), Self::Symbol(rhs)) => lhs == rhs,
+            (Self::Fun(_), Self::Fun(_)) => false,
+            _ => true
+        }
+    }
+}
